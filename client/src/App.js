638ae5cf928837b0bf539bcad9a9d8f9d07257d4
JoysonAJ <joysondsouzaaj@gmail.com>
2022-12-25 17:06:03 +0530

    done with main components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+import '../src/Stylesheet/App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Main from "./components/Main";
+import Quiz from "./components/Quiz";
+import Result from "./components/Result";
 
 function App() {
+
+    const router = createBrowserRouter([
+        {
+            path:'/',
+            element:<Main />
+        },
+
+        {
+            path:'/quiz',
+            element: <Quiz />
+        },
+
+        {
+            path:'/result',
+            element:<Result />
+        },
+    ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+
+
+   {/*<h1>*/}
+   {/*    React App*/}
+   {/*</h1>*/}
+
+       <RouterProvider router={router} />
+   </>
   );
 }
 
